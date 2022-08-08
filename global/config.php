c20b67629683429f12749a922604135e33aa3058
Sergio <sergioarce777@hotmail.com>
2022-08-08 18:51:33 -0500

    tienda online v1

---
+++ global/config.php
@@ -0,0 +1,11 @@
+<?php
+    define('KEY', 'arce7');
+    define('COD', 'AES-128-ECB');
+
+
+    define('HOST', 'localhost:33065');
+    define('DB', 'tiendaonline');
+    define('USER', 'root');
+    define('PASSWORD', '');
+
+?>
---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+</div>
+    <div class="vh-50 col-12 text-center bg-warning fixed-bottom text-secondary">
+        Sergio Gonzalez 2022
+    </div>
+</body>
+</html>
